64fd47ab1a79c36c53880052f4253331dc8fedc4
Markus Grimm <grimm@magazino.eu>
2023-10-07 22:18:14 +0200

    Update landing page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,5 +1,13 @@
 <template>
   <h6>{{ $t('page.title') }}</h6>
+
+  Auf dieser Seite findest Du alle Informationen zu den Streuobstwiesen in Hilgertshausen.
+  <ul>
+    <li>die Position der Wiesen im Gemeindegebiet</li>
+    <li>eine Auflistung aller Bäume auf den jeweiligen Wiesen</li>
+    <li>Informationen zu den Frucht-Sorten</li>
+    <li>Bilder einzelner Bäume und deren Früchte</li>
+  </ul>
 </template>
 
 <script lang="ts">
